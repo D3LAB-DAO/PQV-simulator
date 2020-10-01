1cb6fe510680f41672c0e0041112a8bf0bf42f68
lukepark327 <lukepark327@gmail.com>
2020-10-01 19:09:44 +0900

    fix simul.sh files to support bash

--- simul-1.sh
+++ simul-1.sh
@@ -1,7 +1,7 @@
-for i in `seq 1.0 0.1 3.1`; do  # 3.0
-    python simulator-1.py --pqvMethod='power' --power=$i ${@:1}
+for i in `seq 1.0 0.1 3.0`; do  # 3.0
+    python simulator-1.py --pqvMethod='power' --power=$i "$@"
 done
 
 # for i in `seq 10 10 100`; do
-#     python simulator-1.py --pqvMethod='window' --window=$i ${@:1}
+#     python simulator-1.py --pqvMethod='window' --window=$i "$@"
 # done
